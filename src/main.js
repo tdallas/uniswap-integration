0fd5e01b51c07167f395ccae5578dd02a809c50b
tdallas <tdallas@itba.edu.ar>
2020-11-16 19:10:20 -0300

    feat: added factory abi, connect with web3 and extract pair address

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
-const { ChainId, Token, WETH, ETHER, Fetcher, Route, Percent } = require('@uniswap/sdk');
+const { ChainId, Token, WETH, ETHER, Fetcher, Route, Trade, TokenAmount, TradeType, Percent } = require('@uniswap/sdk');
+const Web3 = require('web3');
+const { factoryABI, factoryAddress } = require('./uniswap_utils');
 
 // IMPORTANT NOTE
 // Internally, the SDK uses WETH, as all Uniswap V2 pairs use WETH under the hood. 
@@ -19,15 +21,36 @@ const getPair = async () =>
         .then(pair => pair)
         .catch(err => { console.log('rompio trayendo el par', err); });
 
+const web3 = new Web3(new Web3.providers.HttpProvider(
+    'https://ropsten.infura.io/v3/f1717b26161c4899b30896bae5c18bb2'
+));
+
+const factoryContract = new web3.eth.Contract(factoryABI, factoryAddress)
+
 const start = async () => {
     const pair = await getPair();
+    const pairTokenAddress = pair.liquidityToken.address;
+
+    console.log('pair address', pairTokenAddress);
+
     const route = new Route([pair], WETH[chainIdRopsten])
     const midPrice = route.midPrice.toSignificant(6);
     console.log('mid Price for DAI-WETH', midPrice);
+    
     //lets say we want to trade 0.1 weth for as much dai as possible
     const amountIn = '100000000000000000' // 0.1 WETH
 
+    const slippageTolerance = new Percent('50', '10000') // 50 bips, or 0.50%
     const trade = new Trade(route, new TokenAmount(WETH[DAI.chainId], amountIn), TradeType.EXACT_INPUT)
+    const amountOutMin = trade.minimumAmountOut(slippageTolerance).raw // needs to be converted to e.g. hex
+    const path = [WETH[DAI.chainId].address, DAI.address]
+    const to = '' // should be a checksummed recipient address
+    const deadline = Math.floor(Date.now() / 1000) + 60 * 20 // 20 minutes from the current Unix time
+    const value = trade.inputAmount.raw // // needs to be converted to e.g. hex
+
+    // we will send a tx to this function
+    // function swapExactETHForTokens(uint amountOutMin, address[] calldata path, address to, uint deadline)
+
 };
 
 
